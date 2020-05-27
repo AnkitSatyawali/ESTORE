5e6dff03860cb1228ffe44ae05336bf95a06c144
Ankit Satyawali <ankitsatywali@gmail.com>
2020-05-27 11:40:35 +0530

    Code Pushed

---
+++ includes/footer.php
@@ -0,0 +1,5 @@
+<footer class="main-footer">
+    <div class="container" style="text-align:center">
+      <strong>Code With <span style="color:red">&#10084;</span> and &#9749; By Ankit Satyawali<strong>
+    </div>
+</footer>
